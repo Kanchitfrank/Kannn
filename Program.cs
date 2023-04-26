2239f65b35ef839cee01d029a843699627849dc5
Kanchit <kanfrankchot@gmail.com>
2023-04-26 14:32:43 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Enter the number of Shop: ");
+        int Shop = Convert.ToInt32(Console.ReadLine());
+        bool aa =true;
+        char[] underscores = new char[Shop];
+
+        for (int i = 0; i < Shop; i++)
+        {
+            underscores[i] = '_';
+        }
+
+        for (int i = 0; i < Shop; i++)
+        {
+            Console.Write(underscores[i] + " ");
+        }
+
+        Console.WriteLine();
+        while(aa){
+             int position = Convert.ToInt32(Console.ReadLine());
+
+        if (position >= 1 && position <= Shop)
+        {
+            underscores[position - 1] = 'x';
+        }
+        else
+        {
+
+        }
+
+        for (int i = 0; i < Shop; i++)
+        {
+            Console.Write(underscores[i] + " ");
+        }
+        }
+       
+
+        Console.ReadLine();
+    }
+}
